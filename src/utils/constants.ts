103a5e7edc685134d8002be075779147c0728f35
Amilcar Erazo <ajose.erazo@gmail.com>
2023-04-09 22:48:46 -0500

    CHanging api

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const STELLAR_CORE = "http://localhost:3001";
+export const STELLAR_CORE = "https://stellar-core.getmeru.com";
 export const SEP30_URL = "http://localhost:3000";
 export const STELLAR_NETWORK_PASSPHRASE = "Test SDF Network ; September 2015";
 export const SEP_10_WEB_AUTH_SERVER = "https://anchor-testnet.stablecop.com/auth";
