adf0545be1b3cf3416fe5e4f7d9a859d0a733c29
Amilcar Erazo <ajose.erazo@gmail.com>
2023-04-17 10:24:15 -0500

    Fixing hosts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const STELLAR_CORE = "http://localhost:3001";
-export const SEP30_URL = "http://localhost:3000";
+export const STELLAR_CORE = "https://stellar-core.getmeru.com";
+export const SEP30_URL = "https://kms-testnet.getmeru.com";
 export const STELLAR_NETWORK_PASSPHRASE = "Test SDF Network ; September 2015";
 export const SEP_10_WEB_AUTH_SERVER = "https://anchor-testnet.stablecop.com/auth";
 export const SEP_10_SIGNING_KEY =
